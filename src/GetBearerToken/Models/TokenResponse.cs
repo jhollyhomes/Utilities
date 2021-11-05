b33317d0d20f4070cd4fb72d6189c5f9bf73f534
James Hollyhomes <jamie.hollyhomes@hotmail.co.uk>
2021-11-05 15:44:04 +0000

    update

---
+++ src/GetBearerToken/Models/TokenResponse.cs
@@ -0,0 +1,6 @@
+    public class TokenResponse
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+    }
---
+++ src/GetBearerToken/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Attempting to get token");
+
+var values = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("client_id", "client"),
+                    new KeyValuePair<string, string>("client_secret", "secret"),
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("username", "jdoe@mail.com"),
+                    new KeyValuePair<string, string>("password", "password")
+                };
+
+var encodedContent = new FormUrlEncodedContent(values);
+    
+using var client = new HttpClient();    
+
+var result = client.PostAsync("https://www.identityserver.com/connect/token", encodedContent).Result;
+var response = result.Content.ReadAsStringAsync().Result;
+var tokenResponse = JsonSerializer.Deserialize<TokenResponse>(response);
+
+Console.WriteLine($"response {response}");
+Console.WriteLine($"token {tokenResponse.access_token}");
